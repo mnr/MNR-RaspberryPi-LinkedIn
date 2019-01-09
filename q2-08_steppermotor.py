b8db6400be0da989ded61d29dead7c5d48574d64
Mark Niemann-Ross <mark@niemannross.com>
2019-01-08 17:59:08 -0800

    tweak steps and delay

--- q2-08_steppermotor.py
+++ q2-08_steppermotor.py
@@ -12,6 +12,9 @@ L293D_3A = 18
 L293D_4A = 16
 
 GPIO.setup(L293D_enable, GPIO.OUT)
+#pulseWidthMod = GPIO.PWM(L293D_enable, 50)
+#pulseWidthMod.start(1)
+
 GPIO.setup(L293D_1A, GPIO.OUT)
 GPIO.setup(L293D_2A, GPIO.OUT)
 GPIO.setup(L293D_3A, GPIO.OUT)
@@ -51,10 +54,12 @@ def setStep(w1, w2, w3, w4):
 while True:
   # delay = raw_input("Delay between steps (milliseconds)?")
   # steps = raw_input("How many steps forward? ")
-  delay = 1
+  delay = 5
   steps = 100
   forward(int(delay) / 1000.0, int(steps))
   # steps = raw_input("How many steps backwards? ")
   steps = 100
   backwards(int(delay) / 1000.0, int(steps))
   print("one cycle")
+  
+GPIO.cleanup()
